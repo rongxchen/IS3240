806ee3291019ea0e21517426f429bd432f86e868
rongxchen <chenrongxin20020102@gmail.com>
2023-09-11 14:13:41 +0800

    v1

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=["GET"])
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return render_template("index.html")
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0", port=8000, debug=True)
