475d88dc8507b9227b149939eb0ebfd5b3f71c88
rongxchen <chenrongxin20020102@gmail.com>
2023-10-07 18:18:59 +0800

    frontend init

--- general_config.py
+++ general_config.py
@@ -4,6 +4,7 @@ from flask import request
 from utils import jwt_util
 
 db_path = "sqlite:///" + os.path.join(os.path.dirname(__file__), "sqlite3.db")
+resource_path = os.path.join(os.path.dirname(__file__), "resources")
 
 def result(code: int, message: str, data: any = None):
     return {
@@ -15,7 +16,7 @@ def result(code: int, message: str, data: any = None):
 def token_required(f):
     @wraps(f)
     def decorator(*args, **kwargs):
-        token = request.headers["Authorization"]
+        token = request.headers["Authorization"] if "Authorization" in request.headers else None
         if not token:
             return result(403, "unauthorized")
         verified, resp = jwt_util.verify_token(token)
