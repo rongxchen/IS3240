69aa2b3d2e642925b6cf0dea662a2acb7d022b42
rongxchen <chenrongxin20020102@gmail.com>
2023-10-11 21:28:04 +0800

    improve stock price fetching

--- controllers/stock_controller.py
+++ controllers/stock_controller.py
@@ -1,8 +1,5 @@
-import os
-import pandas as pd
-from datetime import datetime
 from services import stock_service
-from general_config import result, token_required, resource_path
+from general_config import result, token_required
 from flask import Blueprint, make_response, request
 
 stock_api = Blueprint("stock_api", __name__)
@@ -16,7 +13,7 @@ def search_stock(user_id, symbol, market):
     :param market: US / HK
     :return: a list of stock info
     """
-    stock_list = stock_service.search(symbol, market!="ALL", market)
+    stock_list = stock_service.search(symbol, market)
     return result(200, "success", stock_list)
 
 @stock_api.get("/api/stocks/price/<string:symbol>/<string:market>/<string:k_type>")
@@ -32,38 +29,22 @@ def get_stock_price(user_id, symbol, market, k_type):
     stock_price = stock_service.get_stock_price(symbol, market, k_type)
     return result(200, "success", stock_price)
 
-# @stock_api.post("/api/stocks/price/download")
-# @token_required
-# def download_price_data(user_id):
-#     """ download price data .xlsx
-#     :param user_id: /
-#     :return: binary data of Excel file for download
-#     """
-#     body = request.json
-#     symbol = body["symbol"]
-#     market = str(body["market"]).upper()
-#     k_type = str(body["k_type"]).upper()
-#     date = "".join(datetime.now().strftime("%Y-%m-%d").split("-"))
-#     # join the path
-#     dir_path = os.path.join(resource_path, "csv", "stock_price")
-#     prefix = f"{symbol}-{market}-{k_type}"
-#     file_name = f"{prefix}-{date}.xlsx"
-#     # check if the path directory exists
-#     if not os.path.exists(dir_path):
-#         os.makedirs(dir_path)
-#     path = os.path.join(dir_path, file_name)
-#     # if the price.xlsx does not exist, create a new one
-#     if not os.path.exists(path):
-#         remove_all_matched(dir_path, prefix)
-#         price_data = stock_service.TigerTrade.get_stock_price_info(symbol, market, k_type)["price_list"]
-#         df = pd.DataFrame(price_data)
-#         df.to_excel(path, index=False)
-#         print("data not exist, creating...")
-#     # read the binary data
-#     with open(path, "rb") as file:
-#         binary = file.read()
-#     # send to frontend in response body
-#     response = make_response(binary)
-#     response.headers['Content-Type'] = 'application/octet-stream'
-#     response.headers['Content-Disposition'] = f'attachment; filename="{file_name}"'
-#     return response
+@stock_api.post("/api/stocks/price/download")
+@token_required
+def download_price_data(user_id):
+    """ download price data .xlsx
+    :param user_id: /
+    :return: binary data of Excel file for download
+    """
+    body = request.json
+    symbol = str(body["symbol"]).upper()
+    market = str(body["market"]).upper()
+    k_type = str(body["k_type"]).upper()
+    binary, filename = stock_service.get_csv_binary(symbol, market, k_type)
+    # send to frontend in response body
+    if binary and filename:
+        response = make_response(binary)
+        response.headers['Content-Type'] = 'application/octet-stream'
+        response.headers['Content-Disposition'] = f'attachment; filename="{filename}"'
+        return response
+    return result(400, "error")
--- services/stock_service.py
+++ services/stock_service.py
@@ -1,10 +1,10 @@
 from services.stock_service_tiger_trade import search_stock, get_stock_price_list
 from services.stock_service_helper import find_from_resource
 
-def search(symbol, by_market = False, market = "ALL"):
+def search(symbol, market = "ALL"):
     symbol = str(symbol).upper()
     market = str(market).upper()
-    return search_stock(symbol, by_market, market)
+    return search_stock(symbol, market != "ALL", market)
 
 def get_stock_price(symbol, market, k_type):
     result = find_from_resource(symbol, market, k_type)
@@ -17,7 +17,12 @@ def get_stock_price(symbol, market, k_type):
     return get_stock_price_list(symbol, market, k_type)
 
 def get_csv_binary(symbol, market, k_type):
-    path = find_from_resource(symbol, market, k_type, for_download=True)
-    with open(path, "rb") as file:
-        binary = file.read()
-    return binary
+    try:
+        path = find_from_resource(symbol, market, k_type, for_download=True)
+        if path:
+            with open(path, "rb") as file:
+                binary = file.read()
+            return binary, path.split("\\")[-1]
+    except Exception as e:
+        print(f"exception: {e}")
+    return None, None
